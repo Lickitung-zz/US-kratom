3c1c0ad1657798d5a2a26753da6f520053e6febb
David Inyangetoh <dinyangetoh@gmail.com>
2019-08-29 23:35:00 +0100

    added app layout

---
+++ layouts/app.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>App Layout</h1>
+        <nuxt></nuxt>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "app"
+    }
+</script>
+
+<style scoped>
+
+</style>
